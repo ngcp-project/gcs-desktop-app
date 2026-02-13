8bee9795864f4f70da1182b692b10f9282e50818
Jason <90674857+JasonAndyQuan@users.noreply.github.com>
2026-02-13 13:44:55 -0800

    Feat/weather station (#6)
    * Implemented weather station view w/ placeholder data
    
    * Replaced icons with color-coded svgs
    
    * fix: Moved weather card component under sidebar content
    
    * fix: Replace placeholder text with placeholder variable data
    
    * updated severity logic
    
    ---------
    
    Co-authored-by: PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>

---
+++ src/components/Sidebar/SidebarCards/CameraScreen/WeatherCard.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+  import { Card, CardContent } from "@/components/ui/card";
+  import Overcast from "./WeatherIcons/Overcast.vue";
+  import Wind from "./WeatherIcons/Wind.vue";
+  import Rain from "./WeatherIcons/Rain.vue";
+  import NullData from "./WeatherIcons/NullData.vue";
+  
+  interface WeatherData {
+    overcast: string,
+    wind: number,
+    rain: number
+  }
+  
+  /* 
+    Component | Chill (black) -> moderate (Orange) -> Severe (Red)
+    ---------------------------
+    Overcast  | Sunny -> Cloudy
+    Wind      | 0 mph -> idk mph
+    Rain      | no rain -> heavy rain
+  */
+ const mockWeatherData: WeatherData = {
+   overcast: "sunny",
+   wind: 5, //mph
+   rain: 5 // in/h
+  }; //get from state manager
+  const levels = {
+    light: "black",
+    moderate: "orange",
+    severe: "red"
+  };
+  
+  //compute appropriate color
+  let overcastStatus = levels.light;
+  let windStatus  = (mockWeatherData.wind <= 8) ? levels.light : (mockWeatherData.wind <= 10) ? levels.moderate : levels.severe;
+  let rainStatus = (mockWeatherData.rain <= 0.01) ? levels.light : ( mockWeatherData.rain <= 0.02) ? levels.moderate : levels.severe;
+  const weatherStyles = "flex items-center gap-1";
+</script>
+<template>
+  <Card class="m-2 h-fit bg-sidebar-foreground p-2 text-foreground">
+    <CardContent class="mt-1 flex flex-col items-start space-y-3">
+      <div :class=weatherStyles>
+          <Overcast :color="overcastStatus"/> {{ mockWeatherData.overcast }}
+      </div>
+      <div :class=weatherStyles>
+          <Wind :color="windStatus"/> {{ mockWeatherData.wind }} mph
+      </div>
+      <div :class=weatherStyles>
+        <Rain :color="rainStatus"/> {{ mockWeatherData.rain }}in/h
+      </div>
+    </CardContent>
+  </Card>
+</template>
