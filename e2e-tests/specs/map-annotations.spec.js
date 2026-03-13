fb270aaa99e32b17d1a507bb06f50cfb52270071
Kiyo <kiyo@ncat.cafe>
2026-03-13 02:36:12 -0700

    add edit test, shorter pause duration (and make it a variable)

--- e2e-tests/specs/map-annotations.spec.js
+++ e2e-tests/specs/map-annotations.spec.js
@@ -1,6 +1,6 @@
 import { createDriver, baseUrl } from '../helpers/driver.js';
 import { expect } from 'chai';
-import { By, Origin } from 'selenium-webdriver';
+import { By, Origin, until } from 'selenium-webdriver';
 
 describe("Map annotations", () => {
   let driver;
@@ -18,10 +18,11 @@ describe("Map annotations", () => {
     await driver.quit();
   });
 
+  const pauseDuration = 25;
 
   it("should create a new mission", async () => {
     await driver.findElement(By.css('button.add-mission-button')).click();
-    await driver.sleep(500); // wait a tick to ensure list updates
+    await driver.sleep(pauseDuration); // wait a tick to ensure list updates
     const missionsList = await driver.findElements(By.css('div.missions-list > div'));
     const testMission = missionsList[missionsList.length - 1];
     const missionName = await testMission.findElement(By.css('h3 > input')).getAttribute('value');
@@ -35,7 +36,7 @@ describe("Map annotations", () => {
     expect(missionName).to.equal('new mission');
 
     await testMission.findElement(By.css('button.zones-mission-button')).click();
-    await driver.sleep(500); // wait a tick to ensure list updates
+    await driver.sleep(pauseDuration); // wait a tick to ensure list updates
     await driver.findElements(By.css('div.zones-list > div'));
   });
 
@@ -55,7 +56,7 @@ describe("Map annotations", () => {
     it(`should create a new ${name} zone`, async () => {
       const oldZoneCard = (await driver.findElements(By.css('div.zones-list > div')))[index];
       await oldZoneCard.findElement(By.css('button.add-zone-button')).click();
-      await driver.sleep(500); // wait a tick to ensure list updates
+      await driver.sleep(pauseDuration); // wait a tick to ensure list updates
 
       const zoneCard = (await driver.findElements(By.css('div.zones-list > div')))[index];
       const zoneList = await zoneCard.findElements(By.css('div.zone-content > div'));
@@ -65,23 +66,23 @@ describe("Map annotations", () => {
     });
 
     it(`should set up the created ${name} zone`, async () => {
-      const origin = 200;
-      const offset = 100;
-      const duration = 500;
+      const drawOrigin = 200;
+      const drawOffset = 100;
+      const drawDuration = pauseDuration;
 
       const zoneCard = (await driver.findElements(By.css('div.zones-list > div')))[index];
       const zone = (await zoneCard.findElements(By.css('div.zone-content > div')))[0];
       await zone.findElement(By.css('svg.setup-zone-button')).click();
 
-      await driver.sleep(500); // wait a tick to ensure list updates
       const actions = driver.actions({ async: true });
       await actions
-        .move({ x: origin, y: origin, origin: Origin.VIEWPORT }).click().pause(duration)
-        .move({ x: offset, y: 0, origin: Origin.POINTER }).click().pause(duration)
-        .move({ x: 0, y: offset, origin: Origin.POINTER }).click().pause(duration)
-        .move({ x: 0 - offset, y: 0, origin: Origin.POINTER }).click().pause(duration)
-        .move({ x: 0, y: 0 - offset, origin: Origin.POINTER }).click().pause(duration)
+        .move({ x: drawOrigin, y: drawOrigin, origin: Origin.VIEWPORT }).click().pause(drawDuration)
+        .move({ x: drawOffset, y: 0, origin: Origin.POINTER }).click().pause(drawDuration)
+        .move({ x: 0, y: drawOffset, origin: Origin.POINTER }).click().pause(drawDuration)
+        .move({ x: 0 - drawOffset, y: 0, origin: Origin.POINTER }).click().pause(drawDuration)
+        .move({ x: 0, y: 0 - drawOffset, origin: Origin.POINTER }).click().pause(drawDuration)
         .perform();
+      await actions.clear();
 
       await driver.findElement(By.css('path.leaflet-interactive'));
     });
@@ -95,19 +96,50 @@ describe("Map annotations", () => {
 
       // hidden
       await zone.findElement(By.css('svg.toggle-zone-button')).click();
-      await driver.sleep(500); // wait a tick to ensure list updates
       expect(await zoneMapPath.getAttribute('stroke-opacity')).to.equal('0');
       expect(await zoneMapPath.getAttribute('fill-opacity')).to.equal('0');
 
-      //shown
+      // shown
       await zone.findElement(By.css('svg.toggle-zone-button')).click();
-      await driver.sleep(500); // wait a tick to ensure list updates
       expect(await zoneMapPath.getAttribute('stroke-opacity')).to.equal('1');
       expect(await zoneMapPath.getAttribute('fill-opacity')).to.equal('0.2');
     });
 
     it(`should edit the created ${name} zone`, async () => {
-      // TODO: using setup-zone-button
+      const vertexIndex = 6;
+      const drawOffset = 50;
+      const drawDuration = pauseDuration;
+
+      const zoneCard = (await driver.findElements(By.css('div.zones-list > div')))[index];
+      const zone = (await zoneCard.findElements(By.css('div.zone-content > div')))[0];
+      await zone.findElement(By.css('svg.setup-zone-button')).click();
+
+      const vertexList = await driver.findElements(By.css('div.leaflet-marker-pane > div.leaflet-marker-draggable'));
+
+      const vertex = vertexList[vertexIndex];
+      const oldTransform = await vertex.getCssValue('transform');
+
+      const actions = driver.actions({ async: true });
+      await actions
+        .move({ origin: vertex })
+        .dragAndDrop(vertex, { x: drawOffset, y: drawOffset })
+        .pause(drawDuration)
+        .perform();
+      await actions.clear();
+      const newTransform = await vertex.getCssValue('transform');
+      expect(newTransform).to.not.equal(oldTransform);
+
+      await actions
+        .contextClick(vertex)
+        .pause(drawDuration)
+        .perform();
+      await actions.clear();
+      // the element reference still exists even if the element gets deleted,
+      // but "waiting" for staleness does check if it's removed
+      await driver.wait(until.stalenessOf(vertex), pauseDuration);
+
+      await zone.findElement(By.css('svg.setup-zone-button')).click();
+      await driver.wait(until.stalenessOf(vertexList[0]), pauseDuration); // all items in vertexList should no longer exist at this point
     });
 
     it(`should delete the created ${name} zone`, async () => {
@@ -119,7 +151,7 @@ describe("Map annotations", () => {
 
       const oldLength = zoneList.length;
       await zone.findElement(By.css('svg.delete-zone-button')).click();
-      await driver.sleep(500); // wait a tick to ensure list updates
+      await driver.sleep(pauseDuration); // wait a tick to ensure list updates
       const newZoneCard = (await driver.findElements(By.css('div.zones-list > div')))[index];
       const newLength = (await newZoneCard.findElements(By.css('div.zone-content > div'))).length;
       expect(newLength).to.equal(oldLength - 1);
@@ -129,7 +161,7 @@ describe("Map annotations", () => {
 
   it("should return to missions list", async () => {
     await driver.findElement(By.css('button.back-button')).click();
-    await driver.sleep(500); // wait a tick to ensure list updates
+    await driver.sleep(pauseDuration); // wait a tick to ensure list updates
     await driver.findElements(By.css('div.missions-list > div'));
   });
 
@@ -141,7 +173,7 @@ describe("Map annotations", () => {
 
     const oldLength = missionsList.length;
     await testMission.findElement(By.css('svg.delete-mission-button')).click();
-    await driver.sleep(500); // wait a tick to ensure list updates
+    await driver.sleep(pauseDuration); // wait a tick to ensure list updates
     const newLength = (await driver.findElements(By.css('div.missions-list > div'))).length;
     expect(newLength).to.equal(oldLength - 1);
   });
