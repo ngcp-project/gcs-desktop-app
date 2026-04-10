a237c4252b8d4c9eb8fe4a5d5b0b12541f96da05
Jason Quan <jasonquan30@gmail.com>
2026-04-10 14:22:38 -0700

    OverCast renamed to CloudCoverage. Cloud Coverage states now vary by Icon changes (full sun, partial, full cloud) rather than color. Renamed colored-states to (Unacceptable, Warning, Acceptable)

--- src/components/Sidebar/SidebarCards/CameraScreen/WeatherCard.vue
+++ src/components/Sidebar/SidebarCards/CameraScreen/WeatherCard.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { Card, CardContent } from "@/components/ui/card";
-  import Overcast from "./WeatherIcons/Overcast.vue";
+  import CloudCoverage from "./WeatherIcons/CloudCoverage.vue";
   import Wind from "./WeatherIcons/Wind.vue";
   import Rain from "./WeatherIcons/Rain.vue";
   import NullData from "./WeatherIcons/NullData.vue";
@@ -12,34 +12,41 @@
   }
   
   /* 
-    Component | Chill (black) -> moderate (Orange) -> Severe (Red)
+    Component | Chill (black) -> warning (Orange) -> unacceptable (Red)
     ---------------------------
-    Overcast  | Sunny -> Cloudy
+    CloudCoverage  | Sunny -> Cloudy
     Wind      | 0 mph -> idk mph
     Rain      | no rain -> heavy rain
   */
+
+
  const mockWeatherData: WeatherData = {
    overcast: "sunny",
    wind: 5, //mph
    rain: 5 // in/h
   }; //get from state manager
   const levels = {
-    light: "black",
-    moderate: "orange",
-    severe: "red"
+    acceptable: "fill-black",
+    warning: "fill-yellow-500",
+    unacceptable: "fill-red-500"
   };
-  
+  const cloudCoverageStates = {
+    sun: "sun",
+    partial: "partial",
+    cloud: "cloud"
+  }
+  let cloudCoverageState = cloudCoverageStates.sun;
   //compute appropriate color
-  let overcastStatus = levels.light;
-  let windStatus  = (mockWeatherData.wind <= 8) ? levels.light : (mockWeatherData.wind <= 10) ? levels.moderate : levels.severe;
-  let rainStatus = (mockWeatherData.rain <= 0.01) ? levels.light : ( mockWeatherData.rain <= 0.02) ? levels.moderate : levels.severe;
+  let overcastStatus = levels.warning;
+  let windStatus  = (mockWeatherData.wind <= 8) ? levels.acceptable : (mockWeatherData.wind <= 10) ? levels.warning : levels.unacceptable;
+  let rainStatus = (mockWeatherData.rain <= 0.01) ? levels.acceptable : ( mockWeatherData.rain <= 0.02) ? levels.warning : levels.unacceptable;
   const weatherStyles = "flex items-center gap-1";
 </script>
 <template>
   <Card class="m-2 h-fit bg-sidebar-foreground p-2 text-foreground">
     <CardContent class="mt-1 flex flex-col items-start space-y-3">
       <div :class=weatherStyles>
-          <Overcast :color="overcastStatus"/> {{ mockWeatherData.overcast }}
+          <CloudCoverage :state="cloudCoverageState"/> {{ mockWeatherData.overcast }}
       </div>
       <div :class=weatherStyles>
           <Wind :color="windStatus"/> {{ mockWeatherData.wind }} mph
